91962b9b036c7e4cd7059b0b4ef34694bdb7d5a1
qinjw9709 <82470863+qinjw9709@users.noreply.github.com>
2021-04-18 01:12:06 +0200

    Update topology.py

--- ik2220-assign-phase1-team2/topology/topology.py
+++ ik2220-assign-phase1-team2/topology/topology.py
@@ -10,29 +10,29 @@ class Topology(Topo):
         # initialize base class
         Topo.__init__(self)
 
-        sw1 = self.addSwitch("sw1")
-        sw2 = self.addSwitch("sw2")
-        sw3 = self.addSwitch("sw3")
-        sw4 = self.addSwitch("sw4")
+        sw1 = self.addSwitch("sw1", mac="00:00:00:00:00:11")
+        sw2 = self.addSwitch("sw2", mac="00:00:00:00:00:12")
+        sw3 = self.addSwitch("sw3", mac="00:00:00:00:00:13")
+        sw4 = self.addSwitch("sw4", mac="00:00:00:00:00:14")
         fw1 = self.addSwitch("sw5")
         fw2 = self.addSwitch("sw6")
 
         ### PUBLIC ZONE ###
-        h1 = self.addHost("h1", ip="100.0.0.10/24")
-        h2 = self.addHost("h2", ip="100.0.0.11/24")
+        h1 = self.addHost("h1", ip="100.0.0.10/24", mac="00:00:00:00:00:01")
+        h2 = self.addHost("h2", ip="100.0.0.11/24", mac="00:00:00:00:00:02")
         self.addLink(h1, sw1)
         self.addLink(h2, sw1)
 
         ### PRIVATE ZONE ###
-        h3 = self.addHost("h3", ip="100.0.0.50/24")
-        h4 = self.addHost("h4", ip="100.0.0.51/24")
+        h3 = self.addHost("h3", ip="100.0.0.50/24", mac="00:00:00:00:00:03")
+        h4 = self.addHost("h4", ip="100.0.0.51/24", mac="00:00:00:00:00:04")
         self.addLink(h3, sw3)
         self.addLink(h4, sw3)
 
         ### DEMILITARIZED ZONE ###
-        ws1 = self.addHost("ws1", ip="100.0.0.40/24")
-        ws2 = self.addHost("ws2", ip="100.0.0.41/24")
-        ws3 = self.addHost("ws3", ip="100.0.0.42/24")
+        ws1 = self.addHost("ws1", ip="100.0.0.40/24", mac="00:00:00:00:00:05")
+        ws2 = self.addHost("ws2", ip="100.0.0.41/24", mac="00:00:00:00:00:06")
+        ws3 = self.addHost("ws3", ip="100.0.0.42/24", mac="00:00:00:00:00:07")
         self.addLink(ws1, sw4)
         self.addLink(ws2, sw4)
         self.addLink(ws3, sw4)
@@ -52,6 +52,11 @@ def start_web_servers(net):
         server = net.get(ws)
         server.cmd("python2 -m SimpleHTTPServer 80 &")
 
+def start_arp(net):
+    for host in ["h1","h2","h3","h4"]:
+	client = net.get(host)
+	client.cmd("arp -s 100.0.0.30 00:00:00:00:00:14")
+
 
 def setup():
     # create a topology
@@ -64,7 +69,8 @@ def setup():
 
     # web servers
     start_web_servers(net)
-
+    # virtual ip
+    start_arp(net)
     # start cli
     CLI(net)
 
